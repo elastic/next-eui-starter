820cccd488812ea02af214c12d300671399c16f6
Rory Hunter <roryhunter2@gmail.com>
2020-01-30 13:02:34 +0000

    Update gh-pages to 86d13843

---
+++ _next/static/l6ZxhW3ZPwYwjmuegfxiU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/my-app":[a],"/my-app/[slug]":[a],"/page-2":[a]}}("static\u002Fchunks\u002F1d26d242a31e533c3684dd8e2beedb558aca35d5.cfe953ba5c2dd6ca9533.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
