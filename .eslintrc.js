b4668054e57f87e8dd7b15eff2a1235476c6b1fb
Rory Hunter <roryhunter2@gmail.com>
2021-04-14 20:51:02 +0100

    Upgrade eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,16 +21,7 @@ module.exports = {
     // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended',
 
-    // Uses eslint-config-prettier to disable ESLint rules from
-    // @typescript-eslint/eslint-plugin that would conflict with prettier
-    'prettier/@typescript-eslint',
-
-    'prettier/react',
-    'prettier/standard',
-
-    // Enables eslint-plugin-prettier and eslint-config-prettier. This will
-    // display prettier errors as ESLint errors. Make sure this is always
-    // the last configuration in the extends array.
+    // Enables linting for formatting
     'plugin:prettier/recommended',
   ],
   plugins: ['jsx-a11y', 'prefer-object-spread', 'prettier', 'react-hooks'],
