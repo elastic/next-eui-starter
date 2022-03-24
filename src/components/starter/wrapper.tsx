c79075af1af66a86131dcc1eb83a0777b4548f81
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2022-03-24 14:40:04 +0000

    Warpper styles

--- src/components/starter/wrapper.tsx
+++ src/components/starter/wrapper.tsx
@@ -3,27 +3,18 @@ import { css } from '@emotion/react';
 import Header from './header';
 import GradientBg from './gradient_bg';
 import { useEuiTheme } from '@elastic/eui';
+import { wrapperStyles } from './wrapper.styles';
 
 const Wrapper: FunctionComponent = ({ children }) => {
   const { euiTheme } = useEuiTheme();
+  const styles = wrapperStyles(euiTheme);
 
   return (
     <>
       <Header />
 
       <GradientBg>
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            max-width: 1120px;
-            margin: 0 auto;
-            padding-right: ${euiTheme.size.base};
-            padding-bottom: ${euiTheme.size.xxl};
-            padding-left: ${euiTheme.size.base};
-          `}>
-          {children}
-        </div>
+        <div css={styles.content}>{children}</div>
       </GradientBg>
     </>
   );
